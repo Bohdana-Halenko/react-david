4903b52a43749eae0a1704d98702bf3625b5e379
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2025-06-01 12:02:53 +0300

    01/06

--- src/index.js
+++ src/index.js
@@ -121,3 +121,94 @@ import ReactDOM from 'react-dom/client';
 //     </ul>
 //   )
 // }
+
+
+
+
+// Компоненти
+// const Product = props => (
+//   <>
+//     <img
+//       src="https://content1.rozetka.com.ua/goods/images/big/144249716.jpg"
+//       alt="Macbook"
+//       width="500"
+//     />
+//     <h2>Macbook</h2>
+//     <p>price: 1000</p>
+//     <button type="button">Buy</button>
+//   </>
+// );
+// ReactDOM.createRoot(document.getElementById("root")).render(<Product/>);
+
+
+
+// const App = () => (
+//   <>
+//     <h1>Our products</h1>
+//     <Product
+//             name="Macbook2"
+//             img="https://content1.rozetka.com.ua/goods/images/big/144249716.jpg"
+//             price={  } />
+//   </>
+// );
+
+// const Product = ({img, name, price}) => (
+//     <div>
+//         <h2>{name}</h2>
+//         <img src={img } alt={name } />
+//         <p>Price: {price }</p>
+//         <button type="button">Buy</button>
+//     </div>
+// )
+// ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+
+// значення пропсів за замовчуванням
+// const App = () => (
+//   <>
+//     <h1>Our products</h1>
+//     <Product
+//             img="https://content1.rozetka.com.ua/goods/images/big/144249716.jpg"/>
+//   </>
+// );
+
+// const Product = ({img, name='Macbook', price='1000'}) => (
+//     <div>
+//         <h2>{name}</h2>
+//         <img src={img } alt={name } />
+//         <p>Price: {price }</p>
+//         <button type="button">Buy</button>
+//     </div>
+// )
+// ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+
+
+// choldren
+const Profile = ({ name, email }) => (
+  <div>
+    <p>Name: {name}</p>
+    <p>Email: {email}</p>
+  </div>
+);
+
+const Panel = ({ title, children }) => (
+    <section>
+        {children}
+        <h2>{title}</h2>
+        {children}
+    </section>
+)
+
+const App = () => (
+  <div>
+    <Panel title="User profile">
+      <Profile name="David" email="david@gmail.com" />
+      <Profile name="Dana" email="dana@gmail.com" />
+    </Panel>
+  </div>
+);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+
+//propTypes​ 09.06
