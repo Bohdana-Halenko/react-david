49b1a9f45e2cb225887bda74bbc2d2837667c547
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2025-06-22 18:42:34 +0300

    22.06

---
+++ unit4/src/App.js
@@ -0,0 +1,35 @@
+// import React from "react";
+
+
+// const App = () => {
+//     return <p style={{
+//         color: 'white',
+//         backgroundColor: 'gray',
+//         borderRadius: 4,
+//         padding: '12px 18px',
+//         margin: 10
+//     }}>Hello!</p>;
+// };
+
+// export default App;
+
+
+
+
+
+// 2
+import React from "react";
+import { Alert } from './Alert';
+
+
+const App = () => {
+    return (
+      <>
+        <Alert variant="warning">Please update your email!</Alert>
+        <Alert variant="error">Error!</Alert>
+        <Alert variant='success'>Payment received, thank you!</Alert>
+      </>
+    );
+}
+
+export default App;
