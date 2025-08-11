50919e87a0d003bf59a1bad1802af388227b3914
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2025-08-11 18:10:21 +0300

    Update App.js

--- unit8/src/App.js
+++ unit8/src/App.js
@@ -1,6 +1,8 @@
 // import logo from './logo.svg';
 // import './App.css';
 
+const { Component } = require("react");
+
 // function App() {
 //   return 
 // }
@@ -8,25 +10,71 @@
 // export default App;
 
 
-class Count extends Component{
+// class Count extends Component{
 
-  static defaultProps ={
-    step: 1,
-    initialValue: 0,
-  }
+//   static defaultProps ={
+//     step: 1,
+//     initialValue: 0,
+//   }
 
-    state = {
-      value:  this.props.initialValue,
-  }
+//     state = {
+//       value:  this.props.initialValue,
+//   }
 
-  // 
+//   // 
 
-  // render(){
-  //   return(
-  //     <div>
-  //       <p>{this.state.value}</p>
-  //     </div>
-  //   )
-  // }
-}
-ReactDOM.render(<Count initialValue={10}/>, document.getElementById("root"));
+//   // render(){
+//   //   return(
+//   //     <div>
+//   //       <p>{this.state.value}</p>
+//   //     </div>
+//   //   )
+//   // }
+// }
+// ReactDOM.render(<Count initialValue={10}/>, document.getElementById("root"));
+
+
+
+
+
+
+
+
+
+// state = {
+//   name: "David",
+// }
+
+// // this.state.name = "Bob";
+
+// this.setState({
+//   name: "Bob",
+// })
+
+
+// class Toggle extends Component{
+//   state = {
+//     isOpen: false
+//   };
+//   show =()=> this.setState({isOpen: true});
+//   hide = () => this.setState({isOpen: false});
+
+//   render(){
+//     const {isOpen} = this.state;
+//     const {children} = this.props;
+
+//     return(
+//       <>
+//       <button onClick={this.show}>show</button>
+//       <button onClick={this.hide}>hide</button>
+//       {isOpen && children}
+//       </>
+//     )
+//   }
+// }
+
+
+// const currentState = {a: 5, b: 7, c: 9, d: 11}
+// const newState = {c: 20, d: 25}
+
+// const next = {...currentState, ...newState}
