4187a709263120940eff2f2e116eda2d8eb755ea
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2026-01-07 18:04:06 +0200

    Update App.js

--- unit18/src/App.js
+++ unit18/src/App.js
@@ -208,12 +208,54 @@
 //   );
 // };
 
+// import { useParams } from "react-router-dom";
+
+// const ProductDetails = () =>{
+//   const {productId} = useParams();
+//   return(
+//     )
+// }
+
+//07.01
+// import { useNavigate } from "react-router-dom";
+
+// export const Login = () => {
+//   const navigate = useNavigate();
+
+//   const handleSubmit = async (values) => {
+//     const res = await fakeAPI.login(values);
+//     if (res.success) {
+//       navigate("/profile", { replace: true });
+//     }
+//   };
+
+//   return (
+//     <div>
+//       <h1>Login page</h1>
+//       <LoginForm onSubmit={handleSubmit}/>
+//     </div>)
+// };
 
 
-import { useParams } from "react-router-dom";
 
-const ProductDetails = () =>{
-  const {productId} = useParams();
-  return(
-    )
-}
+// import {Navigate, useState} from "react-router-dom";
+
+// export const Login = () =>{
+// const [isLoginSuccess, setIsLoginSuccess] = useState(false);
+
+// const handleSubmit = async (values) => {
+//       const res = await fakeAPI.login(values);
+//       setIsLoginSuccess(res.success);
+//     };
+
+// if(isLoginSuccess){
+// return <Navigate to="/profile" replace/>}
+
+// return (
+//       <div>
+//         <h1>Login page</h1>
+//         <LoginForm onSubmit={handleSubmit}/>
+//       </div>)
+// }
+
+// https://gomerch.it/products?name=hoodie&color=orange&maxPrice=500
