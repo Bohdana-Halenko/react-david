00e0098013cf26a68a8316666046db5c120f7e5f
Bohdana Halenko <86978157+Bohdana-Halenko@users.noreply.github.com>
2026-03-02 18:09:47 +0200

    +

--- unit18/src/App.js
+++ unit18/src/App.js
@@ -1,386 +1,41 @@
-// function App() {
-//   return
-// }
-
-// export default App;
-
-// import React, { createContext, useContext, useState } from "react";
-// const UserContext = createContext();
-// const useUser = () => useContext(UserContext);
-
-// function UserProvider({children}){
-//   const [isLog, setIsLog] = useState(false);
-//   const [username, setUsername] = useState(null);
-
-//   const logIn = () =>{
-//     setIsLog(true);
-//     setUsername("David")
-//   }
-
-//   const logOut = () =>{
-//     setIsLog(false);
-//     setUsername(null)
-//   }
-
-//   return(
-//     <UserContext.Provider value={{isLog, logIn, username, logOut}}>
-//       {children}
-//     </UserContext.Provider>
-//   )
-// }
-
-// function UserMenu() {
-//   const { isLog, username, logIn, logOut } = useUser();
-//   return (
-//     <div>
-//       {isLog && <p>{username}</p>}
-//       {isLog ? (
-//         <button onClick = {logOut}>Log out</button>
-//       ) : (
-//         <button onClick = {logIn}>Log in</button>
-//       )}
-//     </div>
-//   );
-// }
-
-// export default function App() {
-//   return (
-//     < UserProvider>
-//       <div>
-//         <h1>useContext hook</h1>
-//         <UserMenu />
-//       </div>
-//     </ UserProvider>
-//   );
-// }
-
-// useRef
-
-// import {useRef} from "react";
-
-// const App = () =>{
-//   const btnRef = useRef();
-
-//   return <button ref={btnRef}>Button with ref</button>
-// }
-
-// import {useEffect, useRef, useState} from "react";
-
-// const App = () => {
-//   const [value, setValue] = useState(0);
-//   const btnRef = useRef();
-
-//   console.log(btnRef.current);
-
-//   useEffect(() =>{
-//     console.log(btnRef.current)
-//   })
-
-//   const handleClick = () => {
-//     console.log(btnRef.current)
-//   }
-
-//   return(
-//     <>
-//       <button onClick = {() => setValue(value+1)}>Update value - re-render</button>
-//       <button ref={btnRef}>Button with ref</button>
-//     </>
-//   )
-// }
-
-// export default App;
-
-// const App = () =>{
-//   const valueRef = useRef(0);
-
-//   useEffect(() => {
-//     console.log(valueRef.current);
-//   })
-
-//   const handleClick = () => {
-//     valueRef.current += 1;
-//   }
-
-//   return <button onClick = {handleClick}>Click</button>
-// };
-
-// export default App;
-
-// import { useRef } from "react";
-
-// const Player = ({source}) => {
-//   const playerRef = useRef(null);
-//   const play = () => playerRef.current.play();
-//   const pause = () => playerRef.current.pause();
-
-//   return (
-//     <div>
-//       <video ref={playerRef} src={source}>Sorry, we have a problem</video>
-//       <div>
-//         <button onClick = {play}>Play</button>
-//         <button onClick={pause}>Pause</button>
-//       </div>
-//     </div>
-//   )
-// }
-
-// const App = () => {
-//   return <Player source="http://media.w3.org/2010/05/sintel/trailer.mp4"/>
-// }
-
-// export default App;
-
-// useMemo
-
-// const memoValue = React.useMemo(() => computeExpensiveValue(a,b), [a, b])
-
-// import {useState, useMemo} from 'react';
-
-// const App = ({someProp}) => {
-//   const [planets, setPlanets] = useState(['Earth', "Mars", "Venus", "Jupiter"]);
-//   const [query, setQuery] = useState("");
-//   const [clicks, setClicks] = useState(0);
-
-//   const filteredPlanets = useMemo(()=>planets.filter(planet => planet.includes(query)), [planets, query])
-
-//   return(
-//     <div>
-//       <div>Some props: {someProp}</div>
-//       <button onClick ={()=>setClicks(clicks+1)}>Numbers of click: {clicks}</button>
-//       <div>
-//       {filteredPlanets.map(planet => (
-//         <div key={planet}>{planet}</div>
-//       ))}
-//     </div>
-//     </div>
-
-//   )
-// }
-
-// export default App;
-
-// useMemo, useCallback практика (п.3)
-// import React, {useState, useMemo, useCallback} from 'react;'
-
-// function App(){
-//   const [numbers, setNumbers] = useState([1, 2, 3]);
-//   const [input, setInput] = useState('');
-//   const [filter, setFilter] = useState('');
-// }
-
-// export default App;
-
-// import {Route, Routes} from "react-router-dom";
-
-// <Routes>
-//   <Route path="/about" element={<About />} />
-//   <Route path="/about" element={<About />} />
-//   <Route path="/about" element={<About />} />
-//   <Route path="/about" element={<About />} />
-// </Routes>;
-
-// 29.12
-
-// import { Route, Routes, NavLink, Link } from "react-router-dom";
-// import Home from "path/Home";
-// import About from "path/About";
-// import Products from "path/Products";
-// import NotFound from "path/NotFound";
-// import ProductDetails from "path/ProductDetails"
-
-// export const App = () => {
-//   return (
-//     <div>
-//       <NavLink>
-//         <Link to="/">Home</Link>
-//         <Link to="/about">About</Link>
-//         <Link to="/products">Products</Link>
-//       </NavLink>
-
-//       <Routes>
-//         <Route path="/" element={<Home/>}/>
-//         <Route path="/about" element={<About/>}/>
-//         <Route path="/products" element={<Products/>}/>
-//         <Route path="/products/:productId" element={<ProductDetails/>}/>
-//         <Route path="*" element={<NotFound/>}/>
-//       </Routes>
-//     </div>
-//   );
-// };
-
-// import { useParams } from "react-router-dom";
-
-// const ProductDetails = () =>{
-//   const {productId} = useParams();
-//   return(
-//     )
-// }
-
-//07.01
-// import { useNavigate } from "react-router-dom";
-
-// export const Login = () => {
-//   const navigate = useNavigate();
-
-//   const handleSubmit = async (values) => {
-//     const res = await fakeAPI.login(values);
-//     if (res.success) {
-//       navigate("/profile", { replace: true });
-//     }
-//   };
-
-//   return (
-//     <div>
-//       <h1>Login page</h1>
-//       <LoginForm onSubmit={handleSubmit}/>
-//     </div>)
-// };
-
-// import {Navigate, useState} from "react-router-dom";
-
-// export const Login = () =>{
-// const [isLoginSuccess, setIsLoginSuccess] = useState(false);
-
-// const handleSubmit = async (values) => {
-//       const res = await fakeAPI.login(values);
-//       setIsLoginSuccess(res.success);
-//     };
-
-// if(isLoginSuccess){
-// return <Navigate to="/profile" replace/>}
-
-// return (
-//       <div>
-//         <h1>Login page</h1>
-//         <LoginForm onSubmit={handleSubmit}/>
-//       </div>)
-// }
-
-// https://gomerch.it/products?name=hoodie&color=orange&maxPrice=500#agreeement
-
-// 12.01.2026 Тема 28
-// {
-//     "pathname": "/products",
-//     "search": "?name=hoodie&color=orange&maxPrice=500",
-//     "hash": "#agreeement",
-//     "state": null,
-//     "key": "random-browser-gen-id"
-// }
-
-// import {useEffect} from 'react';
-// import {useLocation} from 'react-router-dom';
-// import  Analitycs from 'to/analitycs-service';
-
-// const App = () => {
-
-// const location = useLocation();
-
-// useEffect(() => {Analitycs.send(location)}, [location]);
-
-// return <div>...</div>
-// }
-
-// import { useLocation } from "react-router-dom";
-
-// // const Products = () => {
-
-// //     const location = useLocation();
-// // return <Link to={location.state.from}>
-// // Product-1</Link>
-// // }
-
-// const Products = () => {
-//   const location = useLocation();
-//   return <Link state={{from:location}} to="/products/hoodie-1">Product-1</Link>;
-// };
-
-
-// import {lazy, Suspense} from 'react';
-// const Component = lazy(() => import('path/Component'));
-
-// const App =() => {
-// return (
-//     <Suspense>
-//         <Rotes>
-//             <Route/>
-//         </Rotes>
-//     </Suspense>
+// ReactDOM.createPortal(child, container);
+
+// const PopupWindow = () => {
+//   return ReactDOM.createPortal(
+//     <div>Оплата успішна!</div>, document.querySelector('#popup-root')
 //     )
 // }
 
 
+import React from 'react';
+import { ReactDOM } from 'react-dom';
 
+class MyPortalComponent extends React.Component{
+  portalContainer = document.createElement('div');
+  componentDidMount(){
+  document.body.appendChild(this.portalContainer)
+}
+  componentWillUnmount(){
+    document.body.removeChild(this.portalContainer)
+  }
 
+  render(){
+    return ReactDOM.createPortal(
+      this.props.children,
+      this.portalContainer
+      )
+  }
+}
 
+function App(){
+  return(
+    <div>
+      <h1>App Content</h1>
+      <MyPortalComponent>
+        <div>Content in Portal</div>
+      </MyPortalComponent>
+    </div>
+    );
+}
 
-
-
-
-
-// createStore()
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const selectTotalValues = state => {
-//     const a = state.values.a; //5
-//     const b = state.values.b; //10
-//     return a + b;
-// }
-
-// const valueSelector = state => state.some.value;
-
-
-// export const selectTaskCount = state => {
-// const tasks = selectTasks(state);
-// console.log("calculation task count");
-
-// return tasks.reduce(
-//     (count, task) => {
-//         if(task.complated){
-//         count.complated += 1;
-//     } else {
-//         count.active +=1
-//     }
-//     return count;
-//     }, {active: 0, complated: 0}
-//     )
-// }
-
-
-
-// import {createSelector} from "@reduxjs/toolkit";
-
-// const selector = createSelector(
-//         [inputSelector1, inputSelector2, inputSelector3],
-//         (result1, result2, result3) => {
-//         }
-//     )
-
-
-
-import { createSelector } from "@reduxjs/toolkit";
-// Решта селекторів
-export const selectTaskCount = createSelector([selectTasks], tasks => {
- console.log("Calculating task count. Now memoized!");
- return tasks.reduce(
-   (count, task) => {
-     if (task.completed) {
-       count.completed += 1;
-     } else {
-       count.active += 1;
-     }
-     return count;
-   },
-   { active: 0, completed: 0 }
- );
-});
+ReactDOM.render(<App/>, document.getElementById('root'))
